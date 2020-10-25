adabc29f0ca9da2746529ed49c3fe172138c90a4
Calvin9165 <calvin.glenney14@gmail.com>
2020-10-25 16:42:01 -0400

    trying to solve the cash problem with the risk parity backtest

--- backtest.py
+++ backtest.py
@@ -7,6 +7,20 @@ from math import sqrt
 
 from calculating_allocation import allocation, securities_pct
 
+# saving the date column as a seperate series so we can add it back
+dates = securities_pct.index
+
+# removing the Date Index and subsequently the date column from securities DataFrame
+securities_pct.reset_index(inplace=True)
+securities_pct.drop(labels='Date', axis=1, inplace=True)
+
+# setting the pct change value of cash fo 0% for each day
+securities_pct['Cash'] = 0
+
+# removing the Date Index and subsequently the date column from the allocation index
+allocation.reset_index(inplace=True)
+allocation.drop(labels='Date', axis=1, inplace=True)
+
 
 # the frequency with which we rebalance the portfolio
 rebal_freq = 21
@@ -15,18 +29,26 @@ rebal_freq = 21
 invested = 1000
 
 # the dollar value of the portfolio
-portfolio = pd.DataFrame(data=None, columns=['Portfolio'], index=securities_pct.index)
-portfolio.loc[0]['Portfolio'] = invested
+portfolio_value = pd.DataFrame(data=None, columns=['Portfolio'], index=securities_pct.index)
+portfolio_value.iloc[0]['Portfolio'] = invested
+
+# the $ value allocated in each position
+positions = pd.DataFrame(data=None, columns=securities_pct.columns, index=securities_pct.index)
+
 
 
 for t in range(1, len(securities_pct), rebal_freq):
 
-
     t_end = t + rebal_freq
 
+    for position in positions:
+        positions.loc[t: t_end, position] = portfolio_value['Portfolio'][t-1] * (allocation[position][t] * np.cumprod(1 + securities_pct.loc[t: t_end, position]))
+
+    portfolio_value.loc[t: t_end, 'Portfolio'] = np.nansum(positions.loc[t: t_end], axis=1)
 
 
+positions['Portfolio'] = portfolio_value['Portfolio']
 
-
-    pass
+positions.plot()
+plt.show()
 
--- calculating_allocation.py
+++ calculating_allocation.py
@@ -9,24 +9,12 @@ from load_data import securities_pct
 
 # # resetting the Date column to the index for the time being
 securities_pct.set_index(keys='Date', inplace=True)
-#
-# # volatility lookback period
-# vol_period = 63
-#
-# # target volatility contribution for each security in the portfolio
-# target_vol = 0.15 / len(securities_pct.columns)
-#
-# # DataFrame which holds the rolling annualized historical volatility based on vol_period days
-# security_vol = securities_pct.rolling(vol_period).std() * sqrt(252)
-# security_vol.dropna(inplace=True)
-#
-# # creating a DataFrame which stores the theoretical daily allocation to each asset based on the assets volatility
-# # and the portfolio's target volatility
-# allocation = target_vol / security_vol
+
+# volatility lookback period
+lookback = 63
 
 
 def calculate_allocation(df_pct, vol_period, target_vol, rebal_freq):
-
     # DataFrame which holds the rolling annualized historical volatility based on vol_period days
     security_vol = df_pct.rolling(vol_period).std() * sqrt(252)
     security_vol.dropna(inplace=True)
@@ -34,7 +22,7 @@ def calculate_allocation(df_pct, vol_period, target_vol, rebal_freq):
     # creating a DataFrame which stores the theoretical daily allocation to each asset based on the assets volatility
     # and the portfolio's target volatility
     num_stocks = len(security_vol.columns)
-    allocation = target_vol/num_stocks / security_vol
+    allocation = target_vol / num_stocks / security_vol
 
     # only want to keep allocation values for every rebal_freq days, then fill forward until next rebal_freq multiple
     allocation_slice = allocation.iloc[0::rebal_freq]
@@ -42,13 +30,14 @@ def calculate_allocation(df_pct, vol_period, target_vol, rebal_freq):
     final_allocation = pd.DataFrame(data=allocation_slice, index=allocation.index)
     final_allocation.fillna(method='ffill', inplace=True)
 
+    final_allocation['Cash'] = 1 - final_allocation.sum(axis=1)
+
     return final_allocation
 
 
-my_alloc = calculate_allocation(df_pct=securities_pct,
-                                vol_period=63,
-                                target_vol=0.15,
-                                rebal_freq=21)
+allocation = calculate_allocation(df_pct=securities_pct,
+                                  vol_period=lookback,
+                                  target_vol=0.15,
+                                  rebal_freq=21)
 
-my_alloc.plot()
-plt.show()
+securities_pct = securities_pct.iloc[lookback:]
